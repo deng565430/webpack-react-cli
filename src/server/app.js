8f17367b2318c969ed84abbc3a65dd0dd8b21812
deng565430 <562340148@qq.com>
2018-01-12 16:01:26 +0800

    修改路径

--- src/server/app.js
+++ src/server/app.js
@@ -3,6 +3,7 @@ import Koa from 'koa';
 import KoaStatic from 'koa-static';
 import Router from 'koa-simple-router';
 import bodyParser from 'koa-bodyparser';
+import path from 'path';
 
 import InitController from './controllers/InitController';
 import config from './config/main';
@@ -11,7 +12,8 @@ const app = new Koa();
 
 // 使用 bodyParser 和 KoaStatic 中间件
 app.use(bodyParser());
-app.use(KoaStatic(__dirname + '/assets'));
+
+app.use(KoaStatic(path.join(__dirname + '/assets')));
 
 InitController.init(app, Router);
 app.listen(config.port);
