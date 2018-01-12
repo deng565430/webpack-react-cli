7f3c3ecad591bbfb32c29e5ee8409c4c918b45d0
deng565430 <562340148@qq.com>
2018-01-12 14:24:56 +0800

    服务端

---
+++ src/server/app.js
@@ -0,0 +1,20 @@
+// 引入模块
+import Koa from 'koa';
+import KoaStatic from 'koa-static';
+import Router from 'koa-simple-router';
+import bodyParser from 'koa-bodyparser';
+
+import InitController from './controllers/InitController';
+import config from './config/main';
+
+const app = new Koa();
+
+// 使用 bodyParser 和 KoaStatic 中间件
+app.use(bodyParser());
+app.use(KoaStatic(__dirname + '/assets'));
+
+InitController.init(app, Router);
+app.listen(config.port);
+
+console.log('graphQL server listen port: ' + config.port);
+
