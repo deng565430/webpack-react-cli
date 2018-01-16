2f16b08502a4b4907029f53216e5202063adac27
deng565430 <562340148@qq.com>
2018-01-16 18:16:06 +0800

    移除部分数据

--- src/app/redux/reducer.js
+++
@@ -1,6 +0,0 @@
-
-// 合并所有reducer 并且返回
-import { combineReducers } from 'redux';
-import { user } from './user.redux';
-import { userdata } from './userdata.redux';
-export default combineReducers({ user, userdata });
